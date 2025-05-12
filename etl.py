b10931b6c2d4d6cbd3fde83c76e4ea1c05857069
tougheye <mkamin.6696@gmail.com>
2025-05-12 10:07:15 -0700

    Added notes to etl.py

--- etl.py
+++ etl.py
@@ -1,8 +1,8 @@
 import configparser
 import psycopg2
-from sql_queries import copy_table_queries, insert_table_queries, staging_songs_copy
+from sql_queries import copy_table_queries, insert_table_queries
 
-
+# declaring the function to run queries to load the staging tables
 def load_staging_tables(cur, conn):
     for query in copy_table_queries:
         print(f"Executing query: {query}")
@@ -10,7 +10,7 @@ def load_staging_tables(cur, conn):
         conn.commit()
    
 
-
+# declaring the function to run queries to load the Fact and Dimension tables
 def insert_tables(cur, conn):
     for query in insert_table_queries:
         print(f"Executing query: {query}")
