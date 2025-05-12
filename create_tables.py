3770445d6cec8a985e02588a70a145b2fcab7d4c
tougheye <mkamin.6696@gmail.com>
2025-05-12 10:09:30 -0700

    Added comments to create_tables.py

--- create_tables.py
+++ create_tables.py
@@ -2,13 +2,13 @@ import configparser
 import psycopg2
 from sql_queries import create_table_queries, drop_table_queries
 
-
+# declaring functions to execute queries to drop tables
 def drop_tables(cur, conn):
     for query in drop_table_queries:
         cur.execute(query)
         conn.commit()
 
-
+# declaring functions to execute queries to create staging and STAR Schema tables
 def create_tables(cur, conn):
     for query in create_table_queries:
         cur.execute(query)
